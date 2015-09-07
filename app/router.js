47fca1f5b8d6fcc2295c8b6e6b1ca59e774abbf3
zhyq0826 <zhyq0826@gmail.com>
2015-09-07 15:57:24 +0800

    fix add some component

--- app/router.js
+++ app/router.js
@@ -11,6 +11,10 @@ Router.map(function() {
     this.route('ui-button');
     this.route('ui-checkbox');
     this.route('ui-multi-checkbox');
+    this.route('ui-select');
+    this.route('ui-multi-select');
+    this.route('ui-dropdown');
+    this.route('ui-radio');
   });
 });
 
---
+++ tests/unit/routes/ui-multi-select-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:ui-multi-select', 'Unit | Route | ui multi select', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
