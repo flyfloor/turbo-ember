821b64ce05c366a8911911b7dc7897da400016be
jerryshew <jerryshew@gmail.com>
2015-09-08 15:21:19 +0800

    input with label

---
+++ app/components/ui-input.js
@@ -0,0 +1,46 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	tagName: 'div',
+	type: 'text',
+	block: false,
+	label: 'text input',
+	theme: '',
+	color: '',
+	size: '',
+	/**
+	 * Class names to apply to the button
+	 *
+	 * @property {Ember.Array} classNames
+	 */
+	classNames: ['ui', 'field'],
+
+	// -------------------------------------------------------------------------
+	// Actions
+
+	// -------------------------------------------------------------------------
+	// Events
+
+	/**
+	 * Alert external code about the click
+	 *
+	 * @function click
+	 * @returns  {void}
+	 */
+
+	/**
+	 * Class bindings for the button component
+	 *
+	 * @property {Ember.Array} classNameBindings
+	 */
+	classNameBindings: ['theme', 'color', 'size'],
+
+	/**
+	 * Attribute bindings for the button component
+	 *
+	 * @property {Ember.Array} attributeBindings
+	 */
+	initialize: function(argument) {
+		
+	}.on('didInsertElement'),
+});
